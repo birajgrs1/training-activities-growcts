2e4ab7eeb02a2272356f70937cbe6179b41bd78b
birajgrs <birajghorasaine11@gmail.com>
2025-04-18 16:19:24 +0545

    task 3 complete .

---
+++ day3-task/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        primaryColor: "#d5f7fd",
+        secondaryColor: "#0d0100",
+        baseColor: "#7d0901",
+        borderColor: "#aaa",
+        bgNew: "#faf9f5",
+      }
+    },
+  },
+  plugins: [],
+}
+
