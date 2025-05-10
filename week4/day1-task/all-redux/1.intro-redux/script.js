13fe627f755bc99c1e251f0f5bf4ffe3578fd3e5
birajgrs <birajghorasaine11@gmail.com>
2025-05-10 19:59:38 +0545

    understanding concept of redux ...

--- week4/day1-task/all-redux/1.intro-redux/script.js
+++ week4/day1-task/all-redux/1.intro-redux/script.js
@@ -6,7 +6,8 @@ npx parcel index.html
 
 console.dir(createStore);
 
-const initialState = {  //initial-state
+const initialState = {
+  //initial-state
   postCount: 0,
   name: "Biraj",
   age: 22,
@@ -15,66 +16,96 @@ const initialState = {  //initial-state
 /*
   
   let prevState = state;
-  function increment(){
+  function INCREMENT(){
       state = {...state, postCount: state.postCount + 1};
   
   }
   
   // console.log(prevState);
   
-  increment();
+  INCREMENT();
   console.log(state);
   // console.log(prevState);
-  increment();
+  INCREMENT();
   console.log(state);
-  increment();
+  INCREMENT();
   console.log(state);
   // console.log(prevState);
   
   */
 
-function reducer(state = initialState, action) {    //reducer(currentState, action)
+const INCREMENT = "post/INCREMENT";
+const DECREMENT = "post/DECREMENT";
+
+const INCREASEBY = "post/INCREMENTBY";
+
+const DECREASEBY = "post/DECREMENTBY";
+
+function reducer(state = initialState, action) {
+  //reducer(currentState, action)
   console.log(action);
 
-  if (action.type === "post/increment") {
+  /*
+  if (action.type === INCREMENT) {
     return { ...state, postCount: state.postCount + 1 };
-  } else if (action.type === "post/decrement") {
+  } else if (action.type === DECREMENT) {
     return { ...state, postCount: state.postCount - 1 };
-  } else if (action.type === "post/incrementBy") {
+  } else if (action.type === increaseBY) {
     console.log(action);
     return { ...state, postCount: state.postCount + action.payload };
-  } else if (action.type === "post/decrementBy") {
+  } else if (action.type === decreaseBY) {
     console.log(action);
     return { ...state, postCount: state.postCount - action.payload };
   }
   return state;
+  */
+
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, postCount: state.postCount + 1 };
+    case DECREMENT:
+      return { ...state, postCount: state.postCount - 1 };
+    case INCREASEBY:
+      return { ...state, postCount: state.postCount + action.payload };
+    case DECREASEBY:
+      return { ...state, postCount: state.postCount - action.payload };
+    default:
+      return state;
+  }
 }
 
 const store = createStore(reducer);
-// console.log(store);
-console.log(store.getState());
-store.dispatch({type: 'post/increment'});
-console.log(store.getState());
-store.dispatch({type: 'post/incrementBy', payload:5});
-console.log(store.getState());
+console.log(store);
+// console.log(store.getState());
+
+// subscribe
+store.subscribe(() => {
+  console.log(store.getState());
+});
+// console.log(store.getState());
+store.dispatch({ type: INCREMENT });
+store.dispatch({ type: DECREMENT });
+store.dispatch({ type: INCREASEBY, payload: 5 });
+store.dispatch({ type: DECREASEBY, payload: 2 });
+// console.log(store.getState());
 
 /*
 console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/increment" });
+reduxState = reducer(reduxState, { type: "post/INCREMENT" });
 console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/increment" });
+reduxState = reducer(reduxState, { type: "post/INCREMENT" });
 console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/increment" });
-console.log(reduxState);
-
-reduxState = reducer(reduxState, { type: "post/incrementBy", payload: 10 });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/incrementBy", payload: 10 });
+reduxState = reducer(reduxState, { type: "post/INCREMENT" });
 console.log(reduxState);
 
-reduxState = reducer(reduxState, { type: "post/decrementBy", payload: 5 });
+reduxState = reducer(reduxState, { type: "post/INCREMENTBY", payload: 10 });
+console.log(reduxState);
+reduxState = reducer(reduxState, { type: "post/INCREMENTBY", payload: 10 });
 console.log(reduxState);
 
-reduxState = reducer(reduxState, { type: "post/decrement" });
+reduxState = reducer(reduxState, { type: "post/DECREMENTBY", payload: 5 });
+console.log(reduxState);
+
+reduxState = reducer(reduxState, { type: "post/DECREMENT" });
 console.log(reduxState);
 */
