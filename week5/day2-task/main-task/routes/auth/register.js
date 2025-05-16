7bae82fada8eb37998c73bcae3ab570c10d3ef91
birajgrs <birajghorasaine11@gmail.com>
2025-05-16 18:03:25 +0545

    task completed...

--- week5/day2-task/main-task/routes/auth/register.js
+++ week5/day2-task/main-task/routes/auth/register.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
+// const crypto = require("crypto");
 const users = require("../../index").users;
 
 function isValidEmail(email) {
@@ -30,6 +31,7 @@ router.post("/", async (req, res) => {
     return res.status(400).json({ message: "User already exists." });
   }
 
+  // const hashedPassword =  crypto.createHash("sha256").update(password).digest("hex");
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = { name, email, password: hashedPassword };
   users.push(newUser);
